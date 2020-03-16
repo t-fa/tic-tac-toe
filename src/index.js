61c3c285de8bf71c634afd0358f2a906ec5d9784
t-fa <thomas.fattah@gmail.com>
2020-03-16 14:54:28 -0400

    Added handleClick method

--- src/index.js
+++ src/index.js
@@ -23,6 +23,12 @@ class Square extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
       return (
         <Square
